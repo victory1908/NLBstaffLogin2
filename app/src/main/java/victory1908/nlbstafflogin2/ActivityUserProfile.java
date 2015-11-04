a2395e817326e93c7a88a5d035b81b289fa073b2
victory1908 <victory1908@gmail.com>
2015-11-04 18:26:05 +0800

    6th

--- app/src/main/java/victory1908/nlbstafflogin2/ActivityUserProfile.java
+++ app/src/main/java/victory1908/nlbstafflogin2/ActivityUserProfile.java
@@ -4,22 +4,107 @@ package victory1908.nlbstafflogin2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ActivityUserProfile extends AppCompatActivity {
     private TextView textView;
+
+    public final String KEY_BEACON_UUID = "beaconUUID";
+
+//    TextView eventid = (TextView) findViewById(R.id.Event_Today);
+
+    private String beaconUUID = "F94DBB23-2266-7822-3782-57BEAC0952AC";
+
+    private JSONArray result;
+
+    //Initializing the ArrayList
+    private ArrayList<String> event = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        textView = (TextView) findViewById(R.id.textViewUsername);
+        textView = (TextView) findViewById(R.id.textViewStaffID);
 
         Intent intent = getIntent();
 
         textView.setText("Welcome User " + intent.getStringExtra(LoginActivity.KEY_STAFFID));
+
+        getData();
+    }
+
+    // Display eventID from beaconUUID
+    private void getData() {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Config.EVENT_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject j = null;
+                        try {
+                            //Parsing the fetched Json String to JSON Object
+                            j = new JSONObject(response);
+
+                            //Storing the Array of JSON String to our JSON Array
+                            result = j.getJSONArray(Config.JSON_ARRAY);
+
+                            //Calling method getEventID to get the eventID from the JSON Array
+                            getEventtID(result);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(ActivityUserProfile.this,error.toString(),Toast.LENGTH_LONG ).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> map = new HashMap<String,String>();
+                map.put(KEY_BEACON_UUID, beaconUUID);
+                return map;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private void getEventtID(JSONArray j) {
+        //Traversing through all the items in the json array
+        for (int i = 0; i < j.length(); i++) {
+            try {
+                //Getting json object
+                JSONObject json = j.getJSONObject(i);
+
+                Toast.makeText(ActivityUserProfile.this,json.getString(Config.KEY_EVENT_ID),Toast.LENGTH_LONG ).show();
+
+                //Adding the EventID to array list
+                event.add(json.getString(Config.KEY_EVENT_ID));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ app/src/main/java/victory1908/nlbstafflogin2/Config.java
@@ -0,0 +1,22 @@
+package victory1908.nlbstafflogin2;
+
+/**
+ * Created by Victory1908 on 04-Nov-15.
+ */
+public class Config {
+    //JSON URL
+    public static final String DATA_URL = "http://simplifiedcoding.16mb.com/Spinner/json.php";
+    public static final String EVENT_URL = "http://vinhvumobile.com/phpconnect/getevent.php";
+
+    //Tags used in the JSON String
+    public static final String KEY_EVENT_ID = "EventID";
+
+    public static final String KEY_BEACON_UUID = "beaconUUID";
+    public String beaconUUID = "F94DBB23-2266-7822-3782-57BEAC0952AC";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_COURSE = "course";
+    public static final String TAG_SESSION = "session";
+
+    //JSON array name
+    public static final String JSON_ARRAY = "result";
+}
